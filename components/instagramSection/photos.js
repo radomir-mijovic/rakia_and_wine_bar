94e33d58109ee20803b1b1879507d5504eeb3ec7
Radomir Mijovic <batomijovic4@gmail.com>
2022-03-08 19:15:12 +0100

    fix: instagram link added to images

--- components/instagramSection/photos.js
+++ components/instagramSection/photos.js
@@ -25,4 +25,4 @@ export const photos = [
     }
 ]
 
-export const instagramUrl = 'https://www.google.com/maps/place/Rakija+and+Wine+bar/@42.27811,18.8372114,15z/data=!4m2!3m1!1s0x0:0xb70f48e00c98c330?sa=X&ved=2ahUKEwibuLS60LT2AhWHP-wKHWauB3kQ_BJ6BAgpEAU'
+export const instagramUrl = 'https://www.instagram.com/rakiawine.budva/'
