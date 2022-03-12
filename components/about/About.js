a403d5e6c912ead189be67b253ed22d342bed93d
Radomir Mijovic <batomijovic4@gmail.com>
2022-03-12 21:09:41 +0100

    fix: changed typo in about text

--- components/about/About.js
+++ components/about/About.js
@@ -10,15 +10,14 @@ const About = () => {
             <h3>
                 Situated in the heart of mesmerizing Old Town and inspired by each Old Town block,
                 Rakia and Wine Bar is taking the area by storm.
-                We embodies the energy of Budva with the laid back atmosphere.
+                We embody the energy of Budva with the laid back atmosphere.
                 Featuring seasonal wines, various rakia shots, and mouthwatering
                 pizza slices, this one-of-a-kind location is the place to be!
-                There’s an art to being the perfect bar for local wine and rakia lovers and Budva locals alike -- and we’ve mastered it.
+                There’s an art to being the perfect bar for local wine and rakia lovers -- and we’ve mastered it.
                 Stay late for a peek at the Rakia and Wine Bar.
                 Or stay all night for delicious drinks, great music, a fun crowd,
                 and a splash of magic.
             </h3>
-
         </AboutStyled>
     );
 };
