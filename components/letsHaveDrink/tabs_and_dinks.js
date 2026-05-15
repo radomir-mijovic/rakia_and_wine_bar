86b837ab659af19bbe20fda0cec710b8f23c244b
Radomir Mijovic <batomijovic4@gmail.com>
2026-05-15 21:41:56 +0200

    chore: sparkling wines

--- components/letsHaveDrink/tabs_and_dinks.js
+++ components/letsHaveDrink/tabs_and_dinks.js
@@ -1,212 +1,201 @@
-
-
 export const tabs_and_drinks = [
-    {
-        imageSrc: '/assets/icons/wine.svg',
-        alt: 'wine',
-        text: 'Wine'
-    },
-    {
-        imageSrc: '/assets/icons/shot2.svg',
-        alt: 'rakia',
-        text: 'Rakia'
-    }
-    ,
-    //{
-    //    imageSrc: '/assets/icons/champagne.svg',
-    //    alt: 'sparkling',
-    //    text: 'Sparkling Wine'
-    //}
-]
-
+  {
+    imageSrc: "/assets/icons/wine.svg",
+    alt: "wine",
+    text: "Wine",
+  },
+  {
+    imageSrc: "/assets/icons/shot2.svg",
+    alt: "rakia",
+    text: "Rakia",
+  },
+  {
+    imageSrc: "/assets/icons/champagne.svg",
+    alt: "sparkling",
+    text: "Sparkling Wine",
+  },
+];
 
 export const wines = [
-    {
-        id: 1,
-        name: 'harmonia 0.7L',
-        alc: 15.5,
-        price: 22.50,
-        glass_price: 4.50,
-        imageSrc: '/assets/images/lipovac_harmonia-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'harmonia',
-        description: 'Dry rose wine in salmon color made in classical Provence style with a light taste, delicate' +
-            ' structure, highlighted by raspberry and wild strawberry notes.'
-    },
-    {
-        id: 2,
-        name: 'concept 0.7L',
-        alc: 15.5,
-        price: 22.50,
-        glass_price: 4.50,
-        imageSrc: '/assets/images/lipovac_concept-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'concept',
-        description: 'Dry red wine with ruby red color, medium structure and notes of ripe fruit that give an amazing juiciness.'
-    },
-    {
-        id: 3,
-        name: 'idea 0.7L',
-        alc: 15.5,
-        price: 26.00,
-        glass_price: 4.50,
-        imageSrc: '/assets/images/lipovac_idea-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'idea',
-        description: 'Dry white wine with beautiful light color, excellent structure and complexity, with elegant,' +
-            ' fresh flower and white peach accents.'
-    },
-    {
-        id: 4,
-        name: 'gea 0.7L',
-        alc: 15.5,
-        price: 31.00,
-        glass_price: 6.00,
-        imageSrc: '/assets/images/lipovac_gea-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'gea',
-        description: 'Dry white wine in amber color, unforgettable fragrance of white flowers and honey with hints' +
-            ' of incense. Dense in the mouth, with a rounded finish and light tannins.'
-    },
-    {
-        id: 5,
-        name: 'tellus 0.7L',
-        alc: 15.5,
-        price: 22.00,
-        glass_price: 6.00,
-        imageSrc: '/assets/images/lipovac_tellus-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'tellus',
-        description: 'Dry red wine with deep ruby color, intensive aroma of cherries, blackberries and forest fruit,' +
-            ' excellent acid structure and balance of tannins.'
-    },
-    {
-        id: 6,
-        name: 'tammuz 0.7L',
-        alc: 15.5,
-        price: 31.00,
-        glass_price: 7.00,
-        imageSrc: '/assets/images/lipovac_tammuz-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'tammuz',
-        description: 'Dry red wine with deep ruby color, intensive aroma of cherries, blackberries and forest fruit,' +
-            ' excellent acid structure and balance of tannins.'
-    },
-    {
-        id: 7,
-        name: 'model 0.7L',
-        alc: 15.5,
-        price: 35.00,
-        glass_price: 6.00,
-        imageSrc: '/assets/images/lipovac_model-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'model',
-        description: 'Dry red wine with garnet color characterized by a good and at the same time elegant structure.' +
-            ' Suitable for long-term bottle aging. This wine with high potential will enrich any wine list and private collection. '
-    }
- ]
+  {
+    id: 1,
+    name: "harmonia 0.7L",
+    alc: 15.5,
+    price: 22.5,
+    glass_price: 4.5,
+    imageSrc: "/assets/images/lipovac_harmonia-.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "harmonia",
+    description:
+      "Dry rose wine in salmon color made in classical Provence style with a light taste, delicate" +
+      " structure, highlighted by raspberry and wild strawberry notes.",
+  },
+  {
+    id: 2,
+    name: "concept 0.7L",
+    alc: 15.5,
+    price: 22.5,
+    glass_price: 4.5,
+    imageSrc: "/assets/images/lipovac_concept-.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "concept",
+    description:
+      "Dry red wine with ruby red color, medium structure and notes of ripe fruit that give an amazing juiciness.",
+  },
+  {
+    id: 3,
+    name: "idea 0.7L",
+    alc: 15.5,
+    price: 26.0,
+    glass_price: 4.5,
+    imageSrc: "/assets/images/lipovac_idea-.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "idea",
+    description:
+      "Dry white wine with beautiful light color, excellent structure and complexity, with elegant," +
+      " fresh flower and white peach accents.",
+  },
+  {
+    id: 4,
+    name: "gea 0.7L",
+    alc: 15.5,
+    price: 31.0,
+    glass_price: 6.0,
+    imageSrc: "/assets/images/lipovac_gea-.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "gea",
+    description:
+      "Dry white wine in amber color, unforgettable fragrance of white flowers and honey with hints" +
+      " of incense. Dense in the mouth, with a rounded finish and light tannins.",
+  },
+  {
+    id: 5,
+    name: "tellus 0.7L",
+    alc: 15.5,
+    price: 22.0,
+    glass_price: 6.0,
+    imageSrc: "/assets/images/lipovac_tellus-.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "tellus",
+    description:
+      "Dry red wine with deep ruby color, intensive aroma of cherries, blackberries and forest fruit," +
+      " excellent acid structure and balance of tannins.",
+  },
+  {
+    id: 6,
+    name: "tammuz 0.7L",
+    alc: 15.5,
+    price: 31.0,
+    glass_price: 7.0,
+    imageSrc: "/assets/images/lipovac_tammuz-.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "tammuz",
+    description:
+      "Dry red wine with deep ruby color, intensive aroma of cherries, blackberries and forest fruit," +
+      " excellent acid structure and balance of tannins.",
+  },
+  {
+    id: 7,
+    name: "model 0.7L",
+    alc: 15.5,
+    price: 35.0,
+    glass_price: 6.0,
+    imageSrc: "/assets/images/lipovac_model-.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "model",
+    description:
+      "Dry red wine with garnet color characterized by a good and at the same time elegant structure." +
+      " Suitable for long-term bottle aging. This wine with high potential will enrich any wine list and private collection. ",
+  },
+];
 
 export const rakia = [
-    {
-        id: 8,
-        name: 'šljiva 0.7L',
-        alc: 40,
-        price: 27.00,
-        glass_price: 3,
-        imageSrc: '/assets/images/sljiva_barik.png',
-        iconSrc: '/assets/icons/shot2.svg',
-        alt: 'šljiva',
-        description: 'The unity of the potential of pozegaca plum, aged in home-made oak barrels, makes the product unique, balanced, harmonious with a great potential for additional aging. Limited production series. '
-    },
-    {
-        id: 9,
-        name: 'viljamovka 0.7L',
-        alc: 40,
-        price: 27.00,
-        glass_price: 3,
-        imageSrc: '/assets/images/viljamovka.png',
-        iconSrc: '/assets/icons/shot2.svg',
-        alt: 'viljamovka',
-        description: 'Sophisticated product, exceptional aroma and pleasant, long-lasting flavour. Dominant product created as a result of selective harvest of fruit fruits, processed in strictly controlled conditions with controlled fermentation at low temperatures. '
-    },
-    {
-        id: 10,
-        name: 'loza 0.7L',
-        alc: 45,
-        price: 27.00,
-        glass_price: 3,
-        imageSrc: '/assets/images/loza.png',
-        iconSrc: '/assets/icons/shot2.svg',
-        alt: 'loza',
-        description: 'The blend of the well-known varieties Lasta and Muscat Hamburg, is dominated by a distinctly floral aroma, fullness of taste and finely balanced acids that give the product freshness and durability in taste. '
-    },
-    {
-        id: 11,
-        name: 'kajsija 0.7L',
-        alc: 40,
-        price: 27.00,
-        glass_price: 3,
-        imageSrc: '/assets/images/kajsija.png',
-        iconSrc: '/assets/icons/shot2.svg',
-        alt: 'kajsija',
-        description: 'Gentle, fragrant, intoxicating, apricot brandy depicts the symbol of this region. Luxurious scent, the expressive fullness and permanence make our gatherings unforgettable. '
-    },
-    {
-        id: 12,
-        name: 'dunja 0.7L',
-        alc: 40,
-        price: 30.00,
-        glass_price: 3,
-        imageSrc: '/assets/images/dunja_barik.png',
-        iconSrc: '/assets/icons/shot2.svg',
-        alt: 'dunja',
-        description: 'Intense quince scent with finely blended oak finish as a result of aging in specially prepared homemade oak barrels. Strong, full, long-lasting taste, reflects the strength, quality and durability of this product.'
-    },
-]
+  {
+    id: 8,
+    name: "šljiva 0.7L",
+    alc: 40,
+    price: 27.0,
+    glass_price: 3,
+    imageSrc: "/assets/images/sljiva_barik.png",
+    iconSrc: "/assets/icons/shot2.svg",
+    alt: "šljiva",
+    description:
+      "The unity of the potential of pozegaca plum, aged in home-made oak barrels, makes the product unique, balanced, harmonious with a great potential for additional aging. Limited production series. ",
+  },
+  {
+    id: 9,
+    name: "viljamovka 0.7L",
+    alc: 40,
+    price: 27.0,
+    glass_price: 3,
+    imageSrc: "/assets/images/viljamovka.png",
+    iconSrc: "/assets/icons/shot2.svg",
+    alt: "viljamovka",
+    description:
+      "Sophisticated product, exceptional aroma and pleasant, long-lasting flavour. Dominant product created as a result of selective harvest of fruit fruits, processed in strictly controlled conditions with controlled fermentation at low temperatures. ",
+  },
+  {
+    id: 10,
+    name: "loza 0.7L",
+    alc: 45,
+    price: 27.0,
+    glass_price: 3,
+    imageSrc: "/assets/images/loza.png",
+    iconSrc: "/assets/icons/shot2.svg",
+    alt: "loza",
+    description:
+      "The blend of the well-known varieties Lasta and Muscat Hamburg, is dominated by a distinctly floral aroma, fullness of taste and finely balanced acids that give the product freshness and durability in taste. ",
+  },
+  {
+    id: 11,
+    name: "kajsija 0.7L",
+    alc: 40,
+    price: 27.0,
+    glass_price: 3,
+    imageSrc: "/assets/images/kajsija.png",
+    iconSrc: "/assets/icons/shot2.svg",
+    alt: "kajsija",
+    description:
+      "Gentle, fragrant, intoxicating, apricot brandy depicts the symbol of this region. Luxurious scent, the expressive fullness and permanence make our gatherings unforgettable. ",
+  },
+  {
+    id: 12,
+    name: "dunja 0.7L",
+    alc: 40,
+    price: 30.0,
+    glass_price: 3,
+    imageSrc: "/assets/images/dunja_barik.png",
+    iconSrc: "/assets/icons/shot2.svg",
+    alt: "dunja",
+    description:
+      "Intense quince scent with finely blended oak finish as a result of aging in specially prepared homemade oak barrels. Strong, full, long-lasting taste, reflects the strength, quality and durability of this product.",
+  },
+];
 
 export const sparkling_wine = [
-    {
-        id: 13,
-        name: 'Val – brut 0.75L',
-        alc: 12.5,
-        price: 20.00,
-        glass_price: 4,
-        imageSrc: '/assets/images/val_brut-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'Montenegrin Val – brut',
-        description: 'Very smooth and fresh on taste, complemented by yeast notes. It has steady and pleasant bubbles, which make it lively and fresh. It has fine foam, pearly foam.'
-    },
-    {
-        id: 14,
-        name: 'Val – Rose 0.75L',
-        alc: 12.5,
-        price: 25.00,
-        glass_price: 5,
-        imageSrc: '/assets/images/val_rose-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'Montenegrin Val – rose',
-        description: 'Wine of light golden color with green reflection. It is characteristic by its fruity aroma, reminiscent of ripe peaches and pears.'
-    },
-    {
-        id: 15,
-        name: 'prosseco extra dry 0.75L',
-        alc: 12.5,
-        price: 25.00,
-        glass_price: 5,
-        imageSrc: '/assets/images/prosecco_extra_dry-.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'prosseco extra dry',
-        description: 'An exceptional Prosecco Superiore made using the very best grapes from the Gregorio Bortolin family vineyards.  This immensely soft sparkling wine has very fine bubbles and a full, beautiful flavour.  It has delightful aromas of Elderflower blossom, apple and pear.'
-    },
-    {
-        id: 16,
-        name: 'rose extra brut 0.75L',
-        alc: 11,
-        price: 25.00,
-        glass_price: 5,
-        imageSrc: '/assets/images/prosseco_rose.png',
-        iconSrc: '/assets/icons/wine-glass.svg',
-        alt: 'prosseco rose extra brut',
-        description: 'A beautiful and elegant, very dry sparkling Moscato by CaSalina of Valdobbiadane, Northern Italy. Brutissimo has a beautiful rose petal colour with delicate aromas of strawberry jam, peach and citrus fruits.  Dry, yet a smooth feel on the palate with the fruits really evident.'
-    },
-]
+  {
+    id: 13,
+    name: "Motivo Rosé extra dry - Borgo Molino 0.75L",
+    alc: 11,
+    price: 37.5,
+    glass_price: 8.5,
+    imageSrc: "/assets/images/motivo_rose.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "Montenegrin Val – brut",
+    description:
+      "A fine sparkling wine with a bright light pink color; fine, sparkling and long-lasting. The scent is pleasantly fruity with aromas of strawberries and raspberries. The taste is fresh, elegant and inviting.",
+  },
+  {
+    id: 14,
+    name: "Prosecco – Gaudium Ca'Salina 0.75L",
+    alc: 11,
+    price: 32.5,
+    glass_price: 7.5,
+    imageSrc: "/assets/images/ca-salina.png",
+    iconSrc: "/assets/icons/wine-glass.svg",
+    alt: "Montenegrin Val – rose",
+    description:
+      "Sparkling wine with a rich, fine and persistent perlage. The bouquet is broad and refined with hints of vanilla. The flavour is soft, harmonious and lively.",
+  },
+];
+
